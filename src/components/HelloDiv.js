3bfb2f4da58898e1451cef1d2eb7ee417e6136c3
Alexander Fellman <alexanderfellman@The-Wonderful-Wombat-of-Oz.local>
2018-05-28 12:04:23 -0400

    End

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -1,3 +1,14 @@
-const HelloDiv = () => null;
+import React from "react";
 
+const HelloDiv = () => (
+<div>
+<h1>Alex</h1>
+
+<ul>
+<li>Wombats</li>
+<li>Hamburgers</li>
+<li>Puffins</li>
+</ul>
+</div>
+);
 export default HelloDiv;
